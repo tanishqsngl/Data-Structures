8052bba24efc1ec01d2c6e55767f30cc9d5f7005
Tanishq <tanishqsngl@gmail.com>
2018-06-22 00:31:14 +0530

    Done

--- Week 3/ads.cpp
+++ Week 3/ads.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 using std::vector;
 
-int getRevenue(int n, vector<int> first, vector<int>second);
+long long int getRevenue(int n, vector<long double> first, vector<long double>second);
 
 int main()
 {
-    int n, value;
+    int n;
+    long long int value;
     cin>>n;
-    vector<int> first(n);
-    vector<int> second(n);
+    vector<long double> first(n);
+    vector<long double> second(n);
     for(int i=0;i<n;i++)
         cin>>first[i];
     for(int i=0;i<n;i++)
@@ -22,18 +23,18 @@ int main()
     return 0;
 }
 
-int getRevenue(int n, vector<int> first, vector<int>second)
+long long int getRevenue(int n, vector<long double> first, vector<long double>second)
 {
-    int a=0, b=0;
-    int value = 0;
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
+    long double a=0, b=0;
+    long long int value = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
             if(first[j]<= first[j+1]){
                 a= first[j];
                 first[j]= first[j+1];
                 first[j+1]= a;
             }
-            if(second[j]<=second[j+1]){
+            if(second[j]<= second[j+1]){
                 b = second[j];
                 second[j]= second[j+1];
                 second[j+1]= b;
@@ -42,8 +43,7 @@ int getRevenue(int n, vector<int> first, vector<int>second)
     }
 
     for(int i=0;i<n;i++)
-    {
         value = value + first[i]*second[i];
-    }
+
     return value;
 }
